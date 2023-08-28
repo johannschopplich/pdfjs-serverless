dca65b1676a2a45d78d6f79cba847d0e1c813945
Johann Schopplich <mail@johannschopplich.com>
2023-08-28 17:20:41 +0200

    fix: generate `index.d.mts`

--- src/rollup/plugins.ts
+++ src/rollup/plugins.ts
@@ -5,11 +5,10 @@ export function pdfjsTypes(): Plugin {
   return {
     name: 'pdfjs-serverless:types',
     async writeBundle() {
-      await writeFile(
-        'dist/index.d.ts',
-        'export * from \'./types/src/pdf.d.ts\'\n',
-        'utf-8',
-      )
+      const data = 'export * from \'./types/src/pdf.d.ts\'\n'
+
+      for (const filename of ['index.d.ts', 'index.d.mts'])
+        await writeFile(`dist/${filename}`, data, 'utf-8')
     },
   }
 }
