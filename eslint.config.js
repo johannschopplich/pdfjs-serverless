02756ebeb788e7b3aeb13cc80c3e2bdfc83ed581
Johann Schopplich <mail@johannschopplich.com>
2024-03-13 13:06:14 +0100

    chore: update TS config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default await antfu(
-  {
-    ignores: ['tsconfig.json'],
-  },
+  {},
   {
     rules: {
       'unused-imports/no-unused-imports': 'off',
