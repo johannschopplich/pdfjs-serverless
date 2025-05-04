0303ab98e08f7335f707a6167d7553d0d678600d
Johann Schopplich <mail@johannschopplich.com>
2025-05-04 11:30:42 +0200

    feat: export PDF.js types (closes #4)

--- src/rollup/plugins.ts
+++ src/rollup/plugins.ts
@@ -5,15 +5,31 @@ export function pdfjsTypes(): Plugin {
   return {
     name: 'pdfjs-serverless:types',
     async writeBundle() {
-      const data = `
+      const typeExports = `
 import * as PDFJS from './types/src/pdf'
+
+/**
+ * @deprecated Import from \`pdfjs-serverless\` instead.
+ * @example
+ * import { getDocument } from 'pdfjs-serverless'
+ *
+ * const buffer = await fetch('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf')
+ *   .then(res => res.arrayBuffer())
+ *
+ * const document = await getDocument({
+ *   data: new Uint8Array(buffer),
+ *   useSystemFonts: true,
+ * }).promise
+ *
+ * console.log(await document.getMetadata())
+ */
 declare function resolvePDFJS(): Promise<typeof PDFJS>
+
 export { resolvePDFJS }
 export * from './types/src/pdf'
 `.trimStart()
 
-      for (const filename of ['index.d.ts', 'index.d.mts'])
-        await writeFile(`dist/${filename}`, data, 'utf8')
+      await writeFile('dist/index.d.mts', typeExports, 'utf8')
     },
   }
 }
