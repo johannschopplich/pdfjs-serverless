00f9a83ec05bc005578718373461f6e61be1050f
Johann Schopplich <johann@schopplich.com>
2026-04-13 09:06:30 +0200

    fix: add polyfills for Map and Uint8Array methods

--- src/polyfills.mjs
+++ src/polyfills.mjs
@@ -1,4 +1,3 @@
-// Promise polyfill for Node.js < 22 and some browsers.
 if (typeof Promise.withResolvers === 'undefined') {
   Promise.withResolvers = function () {
     let resolve, reject
@@ -8,6 +7,37 @@ if (typeof Promise.withResolvers === 'undefined') {
     })
     return { promise, resolve, reject }
   }
+}
+
+if (typeof Map.prototype.getOrInsertComputed === 'undefined') {
+  // eslint-disable-next-line no-extend-native
+  Object.defineProperty(Map.prototype, 'getOrInsertComputed', {
+    value(key, callbackFn) {
+      if (this.has(key)) {
+        return this.get(key)
+      }
+      const value = callbackFn(key)
+      this.set(key, value)
+      return value
+    },
+    writable: true,
+    configurable: true,
+  })
+}
+
+if (typeof Uint8Array.prototype.toHex === 'undefined') {
+  // eslint-disable-next-line no-extend-native
+  Object.defineProperty(Uint8Array.prototype, 'toHex', {
+    value() {
+      let hex = ''
+      for (let i = 0; i < this.length; i++) {
+        hex += this[i].toString(16).padStart(2, '0')
+      }
+      return hex
+    },
+    writable: true,
+    configurable: true,
+  })
 }
 
 export const polyfills = true
